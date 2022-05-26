0734c27d9e8ba5700377c6e84e7305675f92c770
Mark T. Holder <mtholder@gmail.com>
2022-05-26 12:19:57 -0500

    || exit syntax to keep from continuing after failure

--- treebase-core/db/tb1load/scripts/copy_tb2code.sh
+++ treebase-core/db/tb1load/scripts/copy_tb2code.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# must be executed from data/scripts
+# must be executed from treebase/treebase-core/db/install-prefix/scripts
 
 CMD="cp -R ../../../../treebase-web/target/treebase-web/WEB-INF/lib/ ../tb2jars/"
 echo $CMD
-$CMD
+$CMD || exit
 
 CMD="cp -R ../../../../treebase-core/target/classes/ ../tb2classes/"
 echo $CMD
-$CMD
+$CMD || exit
 
